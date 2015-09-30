294af01f009e87ca014663810d03fe758fb5b94b
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2015-09-30 11:58:58 +0200

    enable es6

---
+++ game.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var settings = require('./package')
+var q = require('q');
+var router = express.Router();
+
+var calculate = () => {
+  var deferred = q.defer();
+  setTimeout(() => {
+    deferred.resolve('text success');
+  }, 1000);
+  return deferred.promise;
+};
+
+function *foo(times) {
+    for(let i = 0; i < times;i++){
+      yield calculate()
+    }
+}
+
+
+router.get('/Info', (req, res, next) => {
+  res.status(200).json({
+    Name : `NodeBot_v${settings.version}`,
+    AvatarUrl : "https://node-os.com/images/nodejs.png",
+    GameType : "AntWars",
+    Description : `node bot in ${settings.version} version`
+  });
+  next();
+});
+
+router.post('/PerformNextMove', (req, res, next) =>{
+
+});
+
+module.exports = router;
