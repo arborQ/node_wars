f5e15c218fb2bc8f98874929dd27e15f4d62f41d
arbor <arbor@o2.pl>
2015-10-06 21:41:07 +0200

    force install

--- deploy.cmd
+++ deploy.cmd
@@ -104,6 +104,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+call !NPM_CMD! install
 call !NPM_CMD! start
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
