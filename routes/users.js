3cd48567c060606d797ef33019d684e2e69502ad
arbor <arbor@o2.pl>
2015-10-06 21:14:55 +0200

    remove babel

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ import { arborBot } from '../bots/arborBot'
 import { bot } from '../bots/botBase'
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
 router.get('/:name', function(req, res, next) {
   var neBot = new  bot(req.params.name);
   res.send(`bot ${neBot.info.Name} calculated name`);
